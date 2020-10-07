58b53f1536a46f2d3171adf4b8a505f7ecbc306b
Osvaldo Hernández <angel.hernandezc16@tectijuana.edu.mx>
2020-10-07 16:47:46 -0700

    Actualizar ejercicio 5

--- Ejercicios Python/Ejercicio 5.py
+++ Ejercicios Python/Ejercicio 5.py
@@ -1,3 +1,3 @@
 print("----------Tabla de potencias del número 2----------")
 for i in range (1,11):
-    print ("Elevado a la potencia", i, "es:", 2**i)
+    print ("Dos elevado a la potencia", i, "es:", 2**i)
