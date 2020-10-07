1ffca222a62ad743899cd6ba7cb235251247fa04
Osvaldo Hernández <angel.hernandezc16@tectijuana.edu.mx>
2020-10-07 16:35:27 -0700

    Actualizar ejercicios 6
    Algunos detalles estéticos

--- Ejercicios Python/Ejercicio 6.py
+++ Ejercicios Python/Ejercicio 6.py
@@ -7,7 +7,6 @@ def clear():
     else:
         os.system("clear")
 clear()
-
 print("--------Programa de conversión--------")
 print("1. Convertir pulgadas a yardas")
 print("2. Convertir píes a pulgadas")
@@ -17,14 +16,14 @@ if opción == "1":
     clear()
     print("--------Convertir pulgadas a yardas--------")
     pulgadas = float(input("Introduzca las pulgadas a convertir: "))
-    print("Las pulgadas convertidas a yardas son:", pulgadas * 0.027777777777778)
+    print("Las pulgadas convertidas a yardas son:", pulgadas * 0.0277778)
 elif opción == "2":
     clear()
     print("--------Convertir píes a pulgadas--------")
     píes = int(input("Introduzca los píes a convertir: "))
     print("Las píes convertidos a pulgadas son:", píes * 12)
 elif opción == "3":
-    print("Hasta luego..")
+    print("Hasta luego...")
 else:
     print("Elija un número entre 1 y 2")
 print ("Fin de la ejecución")
