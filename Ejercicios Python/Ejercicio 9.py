5e967977578fc2d9299b2ccb56d109493b18f3c2
Osvaldo Hernández <angel.hernandezc16@tectijuana.edu.mx>
2020-10-08 11:06:36 -0700

    Ejercicio 9

---
+++ Ejercicios Python/Ejercicio 9.py
@@ -0,0 +1,6 @@
+print("--------Programa para determinar si un número dado es par o impar--------")
+número = int(input("Introduce el número a verificar: "))
+if número % 2 == 0:
+    print(f"El número dado '{número}' es un número PAR")
+else:
+    print(f"El número dado '{número}' es un número IMPAR")
