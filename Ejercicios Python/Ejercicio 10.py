e74941ea85fcd8cfe49b241f71eed6f44133b547
Osvaldo Hernández <angel.hernandezc16@tectijuana.edu.mx>
2020-10-08 16:56:48 -0700

    Ejercicio 10 actualizado

--- Ejercicios Python/Ejercicio 10.py
+++ Ejercicios Python/Ejercicio 10.py
@@ -1,8 +1,12 @@
 print("--------Programa para determinar si una serie de números dados es par o impar--------")
-contador = 0
-for i in range(1, 25 + 1):
-    números = int(input(f"Escriba el número {i}: "))
-    if números % 2 == 0:
-        print(f"El número dado '{números}' es un número PAR")
+números = []
+for i in range(25):
+    núm = int(input(f"Escriba el número {i+1}: "))
+    números.append(núm)
+
+print("------------------Resultado------------------")
+for i in range(25):
+    if números[i] % 2 == 0:
+        print(f"El número dado '{números[i]}' es un número PAR")
     else:
-        print(f"El número dado '{números}' es un número IMPAR")
+        print(f"El número dado '{números[i]}' es un número IMPAR")
